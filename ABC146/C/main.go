a8140bec231329c3ca2249b46716ca414d92f068
ch1aki <ch1aki@EOLIA.localdomain>
2019-12-17 23:35:02 +0900

    wip

--- ABC146/C/main.go
+++ ABC146/C/main.go
@@ -9,11 +9,29 @@ func main() {
 	var a, b, x int
 	fmt.Scanf("%d %d %d", &a, &b, &x)
 
-	n := math.Pow(10, 9)
-	for x <= (a*int(n)+b*int(math.Log10(n))) && n > 0 {
-		n--
+	start := 0
+	end := int(math.Pow(10, 9))
+	var r int
+	for {
+		if end < start {
+			break
+		}
+
+		n := (start + end)/2
+		t := a * n + b * int(math.Log10(float64(n)))
+		fmt.Printf("start:%d\tent:%d\tn:%d\tr:%d\tt:%d\n", start, end, n, r, t) // DEBUG
+
+		if x >= t && t > r {
+			r = n
+		}
+
+		if x > t {
+			start = n
+		} else {
+			end = n
+		}
 	}
 
-	fmt.Println(n)
+	fmt.Println(r)
 
 }
