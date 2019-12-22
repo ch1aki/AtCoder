cb56adb3c63c5d6447eba20e94ba23f6b5e13cb9
ch1aki <ch1aki@EOLIA.localdomain>
2019-12-22 10:10:41 +0900

    ABC049C

---
+++ ABC049C/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var s, t string
+	fmt.Scan(&s)
+
+	fmt.Printf("input: %s\n", s) // DEBUG
+
+	matches := [4]string{
+		"dreamer",
+		"eraser",
+		"dream",
+		"erace",
+	}
+
+	start := 0
+	for {
+		for _, m := range matches {
+			fmt.Printf("DEBUG: %v\n", s[start:len(m)]) // DEBUG
+			if start+len(m) < len(s) && s[start:len(m)-1] == m {
+				t = t + m
+				fmt.Printf("t: %s\n", t) // DEBUG
+				start += len(m)
+				break
+			}
+		}
+		if start > len(s) - 4 {
+			break
+		}
+	}
+
+	fmt.Printf("t: %s\n", t) // DEBUG
+	if s == t {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
